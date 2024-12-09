6a110d22d14dca6c9fe53e492928b927b857b870
royendo <67675319+royendo@users.noreply.github.com>
2024-12-09 13:35:05 -0700

    SVELTE Example

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,5 @@
+<nav style="padding: 1rem; background-color: #f4f4f4;">
+  <a href="/" style="margin-right: 10px;">Home</a>
+  <a href="/page1" style="margin-right: 10px;">Page 1</a>
+  <a href="/page2">Page 2</a>
+</nav>
---
+++ src/routes/page1/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Page 1</h1>
+<p>This is the first page.</p>
